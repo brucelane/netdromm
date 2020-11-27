ed0a8aee6e4739d0a5ff991b09e0a909cfcd6eaa
Bruce LANE <bruce@batchass.fr>
2020-11-27 14:03:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@
 var Promise = global.Promise || require('promise');
 var nodemon = require('nodemon');
 var winston = require('winston');
-var https = require('https');
-var fs = require('fs');
-var cert = fs.readFileSync(process.env.CERT, 'utf8');
-var key = fs.readFileSync(process.env.KEY, 'utf8');
 var server = null;
 var wss = require('./src/ws.js');
 var  port = process.env.port || 8088;
@@ -14,8 +10,7 @@ var  port = process.env.port || 8088;
 function setUpLogger() {
     winston.configure({
         transports: [
-            new (winston.transports.Console)({ colorize: true }),
-            new (winston.transports.File)({ filename: 'netdromm.log' })
+            new (winston.transports.Console)({ colorize: true })
         ]
     });
     return Promise.resolve();
@@ -23,8 +18,8 @@ function setUpLogger() {
 
 function initHttpServer() {
     return new Promise(function( resolve, reject ){
-        if (!cert || !key) reject('Check certs');
-        server = https.createServer({cert: cert , key: key });
+       
+        server = http.createServer();
         server.listen(port, function(err) {
             if (err) return reject(err);
             winston.info('Application server running on port '+ port +'!');
